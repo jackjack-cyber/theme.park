778ae1322b6be70a3ac1dac957ebcf1c3af084f4
jackjack-cyber <81909747+jackjack-cyber@users.noreply.github.com>
2022-08-12 08:39:22 +1000

    Create custom_text_header.js

---
+++ css/addons/unraid/login-page/fallout/js/custom_text_header.js
@@ -0,0 +1,22 @@
+
+ //Custom Text Header //
+ // ACSII slant font
+
+// Needs to be inside <section id="login" class="shadow">
+// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
+
+let custom_text_header = `
+<div class="custom-text-header"> <pre>                     __                             ___       __  
+
+ ██▓    ▄▄▄       ██ ▄█▀▓█████   ▄████  ██▀███   ██▓  ▄████   ▄████ 
+▓██▒   ▒████▄     ██▄█▒ ▓█   ▀  ██▒ ▀█▒▓██ ▒ ██▒▓██▒ ██▒ ▀█▒ ██▒ ▀█▒
+▒██░   ▒██  ▀█▄  ▓███▄░ ▒███   ▒██░▄▄▄░▓██ ░▄█ ▒▒██▒▒██░▄▄▄░▒██░▄▄▄░
+▒██░   ░██▄▄▄▄██ ▓██ █▄ ▒▓█  ▄ ░▓█  ██▓▒██▀▀█▄  ░██░░▓█  ██▓░▓█  ██▓
+░██████▒▓█   ▓██▒▒██▒ █▄░▒████▒░▒▓███▀▒░██▓ ▒██▒░██░░▒▓███▀▒░▒▓███▀▒
+░ ▒░▓  ░▒▒   ▓▒█░▒ ▒▒ ▓▒░░ ▒░ ░ ░▒   ▒ ░ ▒▓ ░▒▓░░▓   ░▒   ▒  ░▒   ▒ 
+░ ░ ▒  ░ ▒   ▒▒ ░░ ░▒ ▒░ ░ ░  ░  ░   ░   ░▒ ░ ▒░ ▒ ░  ░   ░   ░   ░ 
+  ░ ░    ░   ▒   ░ ░░ ░    ░   ░ ░   ░   ░░   ░  ▒ ░░ ░   ░ ░ ░   ░ 
+    ░  ░     ░  ░░  ░      ░  ░      ░    ░      ░        ░       ░ 
+                                                                    
+`;
+document.getElementById("login").innerHTML += custom_text_header
